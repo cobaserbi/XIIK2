720cf9e1e286f1f3c8f330a98ae99370c4380324
Yadi Setiawan <yadisetiawan51@gmail.com>
2024-01-17 13:42:53 +0700

    commit pertama

---
+++ prosesLogin.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    require_once "koneksi.php";
+    
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $status = 0;
+
+    $sqlSelect= mysqli_query($koneksi, "SELECT * FROM akun WHERE username_akun='$username' AND password_akun='$password'");
+    $baris = mysqli_num_rows($sqlSelect);
+    if($baris == 1){
+        while ($do=mysqli_fetch_assoc($sqlSelect)) {
+            $status=$do['status_akun'];
+        }
+        if($status == 1){
+            $_SESSION['user'] = $username;
+            $_SESSION['status'] = "admin";
+            header("Location:beranda_admin.php");
+        }else{
+            echo "ini untuk beranda user";
+        }
+    }else{
+
+    }
+ ?>
